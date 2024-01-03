f2c1cf02d98bcda33e70055533b2127465ec3c92
anky3733 <aniketdphutane@gmail.com>
2024-01-03 19:39:12 +0100

    Updated config files

--- First Task/train.py
+++ First Task/train.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 import yaml
 import os
 
-from helper_functions import create_electrical_grid_data, get_electrical_grid_data, ElectricalGridModel
+from helper_functions import get_electrical_grid_data, ElectricalGridModel
 
 # Get the current working directory
 current_dir = os.path.dirname(os.path.realpath(__file__))
